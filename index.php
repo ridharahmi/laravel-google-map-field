8073c7c101b90280014472c9a4081d83fde41956
ridha rahmi <ridha.rahmi13@gmail.com>
2019-04-13 23:33:16 +0100

    update files js and css

--- index.php
+++ index.php
@@ -2,12 +2,7 @@
 <html>
 <head>
     <title>field maps </title>
-    <style>
-    #map{
-        width: 100%;
-        height: 300px;
-    }
-</style>
+    <link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAw1Wg-f_QsyBl2SexlIGaLhwhIlMM8X9s&libraries=places"></script>
@@ -40,41 +35,7 @@
 
 </div>
 
-<script>
-    var map = new google.maps.Map(document.getElementById('map'),{
-        center:{
-            lat: 27.72,
-            lng: 85.36
-        },
-        zoom:15
-    });
-    var marker = new google.maps.Marker({
-        position: {
-            lat: 27.72,
-            lng: 85.36
-        },
-        map: map,
-        draggable: true
-    });
-    var searchBox = new google.maps.places.SearchBox(document.getElementById('searchmap'));
-    google.maps.event.addListener(searchBox,'places_changed',function(){
-        var places = searchBox.getPlaces();
-        var bounds = new google.maps.LatLngBounds();
-        var i, place;
-        for(i=0; place=places[i];i++){
-            bounds.extend(place.geometry.location);
-            marker.setPosition(place.geometry.location);
-        }
-        map.fitBounds(bounds);
-        map.setZoom(15);
-    });
-    google.maps.event.addListener(marker,'position_changed',function(){
-        var lat = marker.getPosition().lat();
-        var lng = marker.getPosition().lng();
-        $('#lat').val(lat);
-        $('#lng').val(lng);
-    });
-</script>
+<script type="text/javascript" src="style.js"></script>
         
 </body>
 </html>
---
+++ style.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	 var map = new google.maps.Map(document.getElementById('map'),{
+        center:{
+            lat: 27.72,
+            lng: 85.36
+        },
+        zoom:15
+    });
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 27.72,
+            lng: 85.36
+        },
+        map: map,
+        draggable: true
+    });
+    var searchBox = new google.maps.places.SearchBox(document.getElementById('searchmap'));
+    google.maps.event.addListener(searchBox,'places_changed',function(){
+        var places = searchBox.getPlaces();
+        var bounds = new google.maps.LatLngBounds();
+        var i, place;
+        for(i=0; place=places[i];i++){
+            bounds.extend(place.geometry.location);
+            marker.setPosition(place.geometry.location);
+        }
+        map.fitBounds(bounds);
+        map.setZoom(15);
+    });
+    google.maps.event.addListener(marker,'position_changed',function(){
+        var lat = marker.getPosition().lat();
+        var lng = marker.getPosition().lng();
+        $('#lat').val(lat);
+        $('#lng').val(lng);
+    });
+});
+
